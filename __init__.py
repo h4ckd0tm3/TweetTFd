c098b92279e43acfca8f18c6ef6cb8e361cc9695
exokortex <johannes.haring@gmx.net>
2019-02-03 17:54:09 +0100

    update folder name

--- __init__.py
+++ __init__.py
@@ -20,19 +20,19 @@ class TweetnamicChallenge(BaseChallenge):
     id = "tweetnamic"  # Unique identifier used to register challenges
     name = "tweetamic"  # Name of a challenge type
     templates = {  # Handlebars templates used for each aspect of challenge editing & viewing
-        'create': '/plugins/tweettfd/assets/create.html',
-        'update': '/plugins/tweettfd/assets/update.html',
-        'view': '/plugins/tweettfd/assets/view.html',
+        'create': '/plugins/TweetTFd/assets/create.html',
+        'update': '/plugins/TweetTFd/assets/update.html',
+        'view': '/plugins/TweetTFd/assets/view.html',
     }
     scripts = {  # Scripts that are loaded when a template is loaded
-        'create': '/plugins/tweettfd/assets/create.js',
-        'update': '/plugins/tweettfd/assets/update.js',
-        'view': '/plugins/tweettfd/assets/view.js',
+        'create': '/plugins/TweetTFd/assets/create.js',
+        'update': '/plugins/TweetTFd/assets/update.js',
+        'view': '/plugins/TweetTFd/assets/view.js',
     }
     # Route at which files are accessible. This must be registered using register_plugin_assets_directory()
-    route = '/plugins/tweettfd/assets/'
+    route = '/plugins/TweetTFd/assets/'
     # Blueprint used to access the static_folder directory.
-    blueprint = Blueprint('tweettfd', __name__, template_folder='templates', static_folder='assets')
+    blueprint = Blueprint('TweetTFd', __name__, template_folder='templates', static_folder='assets')
 
     @staticmethod
     def create(request):
@@ -258,4 +258,4 @@ def load(app):
     AUTH.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     API = tweepy.API(AUTH)
     CHALLENGE_CLASSES['tweetnamic'] = TweetnamicChallenge
-    register_plugin_assets_directory(app, base_path='/plugins/tweettfd/assets/')
+    register_plugin_assets_directory(app, base_path='/plugins/TweetTFd/assets/')
