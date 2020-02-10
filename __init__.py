51eb8796db1ef851ef941f06a7a097af1fa18bfc
Marcel Schnideritsch <marcel@schnideritsch.at>
2020-02-10 08:47:12 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,41 +2,84 @@ from __future__ import division  # Use floating point for math calculations
 from CTFd.plugins.challenges import BaseChallenge, CHALLENGE_CLASSES
 from CTFd.plugins import register_plugin_assets_directory
 from CTFd.plugins.flags import get_flag_class
-from CTFd.models import db, Solves, Fails, Flags, Challenges, ChallengeFiles, Tags, Teams, Hints
-from CTFd import utils
-from CTFd.utils.migrations import upgrade
+from CTFd.models import (
+    db,
+    Solves,
+    Fails,
+    Flags,
+    Challenges,
+    ChallengeFiles,
+    Tags,
+    Hints,
+)
 from CTFd.utils.user import get_ip
-from CTFd.utils.uploads import upload_file, delete_file
+from CTFd.utils.uploads import delete_file
 from CTFd.utils.modes import get_model
 from flask import Blueprint
 import math
-
+import tweepy
 import socket
 import sys
 import logging
-
-import tweepy
-
-#You have to create a credentials.py file with your tokens and secrets in it
-from .credentials import *
 
 class TweetnamicValueChallenge(BaseChallenge):
     id = "tweetnamic"  # Unique identifier used to register challenges
     name = "tweetnamic"  # Name of a challenge type
     templates = {  # Handlebars templates used for each aspect of challenge editing & viewing
-        'create': '/plugins/TweetTFd/assets/create.html',
-        'update': '/plugins/TweetTFd/assets/update.html',
-        'view': '/plugins/TweetTFd/assets/view.html',
+        "create": "/plugins/TweetTFd/assets/create.html",
+        "update": "/plugins/TweetTFd/assets/update.html",
+        "view": "/plugins/TweetTFd/assets/view.html",
     }
     scripts = {  # Scripts that are loaded when a template is loaded
-        'create': '/plugins/TweetTFd/assets/create.js',
-        'update': '/plugins/TweetTFd/assets/update.js',
-        'view': '/plugins/TweetTFd/assets/view.js',
+        "create": "/plugins/TweetTFd/assets/create.js",
+        "update": "/plugins/TweetTFd/assets/update.js",
+        "view": "/plugins/TweetTFd/assets/view.js",
     }
     # Route at which files are accessible. This must be registered using register_plugin_assets_directory()
-    route = '/plugins/TweetTFd/assets/'
+    route = "/plugins/TweetTFd/assets/"
     # Blueprint used to access the static_folder directory.
-    blueprint = Blueprint('TweetTFd', __name__, template_folder='templates', static_folder='assets')
+    blueprint = Blueprint(
+        "TweetTFd",
+        __name__,
+        template_folder="templates",
+        static_folder="assets",
+    )
+
+    @classmethod
+    def calculate_value(cls, challenge):
+        Model = get_model()
+
+        solve_count = (
+            Solves.query.join(Model, Solves.account_id == Model.id)
+            .filter(
+                Solves.challenge_id == challenge.id,
+                Model.hidden == False,
+                Model.banned == False,
+            )
+            .count()
+        )
+
+        # If the solve count is 0 we shouldn't manipulate the solve count to
+        # let the math update back to normal
+        if solve_count != 0:
+            # We subtract -1 to allow the first solver to get max point value
+            solve_count -= 1
+
+        # It is important that this calculation takes into account floats.
+        # Hence this file uses from __future__ import division
+        value = (
+            ((challenge.minimum - challenge.initial) / (challenge.decay ** 2))
+            * (solve_count ** 2)
+        ) + challenge.initial
+
+        value = math.ceil(value)
+
+        if value < challenge.minimum:
+            value = challenge.minimum
+
+        challenge.value = value
+        db.session.commit()
+        return challenge
 
     @staticmethod
     def create(request):
@@ -47,7 +90,7 @@ class TweetnamicValueChallenge(BaseChallenge):
         :return:
         """
         data = request.form or request.get_json()
-        challenge = TweetnamicChallenge(**data)
+        challenge = TweetnamicValueChallenge(**data)
 
         db.session.add(challenge)
         db.session.commit()
@@ -64,23 +107,23 @@ class TweetnamicValueChallenge(BaseChallenge):
         """
         challenge = TweetnamicChallenge.query.filter_by(id=challenge.id).first()
         data = {
-            'id': challenge.id,
-            'name': challenge.name,
-            'value': challenge.value,
-            'initial': challenge.initial,
-            'decay': challenge.decay,
-            'minimum': challenge.minimum,
-            'description': challenge.description,
-            'category': challenge.category,
-            'state': challenge.state,
-            'max_attempts': challenge.max_attempts,
-            'type': challenge.type,
-            'type_data': {
-                'id': TweetnamicValueChallenge.id,
-                'name': TweetnamicValueChallenge.name,
-                'templates': TweetnamicValueChallenge.templates,
-                'scripts': TweetnamicValueChallenge.scripts,
-            }
+            "id": challenge.id,
+            "name": challenge.name,
+            "value": challenge.value,
+            "initial": challenge.initial,
+            "decay": challenge.decay,
+            "minimum": challenge.minimum,
+            "description": challenge.description,
+            "category": challenge.category,
+            "state": challenge.state,
+            "max_attempts": challenge.max_attempts,
+            "type": challenge.type,
+            "type_data": {
+                "id": TweetnamicValueChallenge.id,
+                "name": TweetnamicValueChallenge.name,
+                "templates": TweetnamicValueChallenge.templates,
+                "scripts": TweetnamicValueChallenge.scripts,
+            },
         }
         return data
 
@@ -98,30 +141,11 @@ class TweetnamicValueChallenge(BaseChallenge):
 
         for attr, value in data.items():
             # We need to set these to floats so that the next operations don't operate on strings
-            if attr in ('initial', 'minimum', 'decay'):
+            if attr in ("initial", "minimum", "decay"):
                 value = float(value)
             setattr(challenge, attr, value)
 
-        Model = get_model()
-
-        solve_count = Solves.query \
-            .join(Model, Solves.account_id == Model.id) \
-            .filter(Solves.challenge_id == challenge.id, Model.hidden == False, Model.banned == False) \
-            .count()
-
-        # It is important that this calculation takes into account floats.
-        # Hence this file uses from __future__ import division
-        value = (((challenge.minimum - challenge.initial) / (challenge.decay ** 2)) * (solve_count ** 2)) + challenge.initial
-
-        value = math.ceil(value)
-
-        if value < challenge.minimum:
-            value = challenge.minimum
-
-        challenge.value = value
-
-        db.session.commit()
-        return challenge
+        return TweetnamicValueChallenge.calculate_value(challenge)
 
     @staticmethod
     def delete(challenge):
@@ -156,12 +180,12 @@ class TweetnamicValueChallenge(BaseChallenge):
         :return: (boolean, string)
         """
         data = request.form or request.get_json()
-        submission = data['submission'].strip()
+        submission = data["submission"].strip()
         flags = Flags.query.filter_by(challenge_id=challenge.id).all()
         for flag in flags:
             if get_flag_class(flag.type).compare(flag, submission):
-                return True, 'Correct'
-        return False, 'Incorrect'
+                return True, "Correct"
+        return False, "Incorrect"
 
     @staticmethod
     def solve(user, team, challenge, request):
@@ -173,53 +197,33 @@ class TweetnamicValueChallenge(BaseChallenge):
         :param request: The request the user submitted
         :return:
         """
-        chal = TweetnamicChallenge.query.filter_by(id=challenge.id).first()
+        challenge = TweetnamicChallenge.query.filter_by(id=challenge.id).first()
         data = request.form or request.get_json()
-        submission = data['submission'].strip()
-
-        Model = get_model()
-
-        solve_count = Solves.query \
-            .join(Model, Solves.account_id == Model.id) \
-            .filter(Solves.challenge_id == challenge.id, Model.hidden == False, Model.banned == False) \
-            .count()
-
-        # It is important that this calculation takes into account floats.
-        # Hence this file uses from __future__ import division
-        value = (
-            (
-                (chal.minimum - chal.initial) / (chal.decay**2)
-            ) * (solve_count**2)
-        ) + chal.initial
-
-        value = math.ceil(value)
-
-        if value < chal.minimum:
-            value = chal.minimum
-
-        chal.value = value
+        submission = data["submission"].strip()
 
         solve = Solves(
             user_id=user.id,
             team_id=team.id if team else None,
             challenge_id=challenge.id,
             ip=get_ip(req=request),
-            provided=submission
+            provided=submission,
         )
         db.session.add(solve)
         db.session.commit()
 
+        TweetnamicValueChallenge.calculate_value(challenge)
+
         try:
-            if solve_count == 0:
+            if _getSolves(challenge) == 0:
                 score = user.get_score(admin=True)
                 place = user.get_place(admin=True)
                 tweet_text = ("{} got first blood on {} and "
                               "is now in {} place with {:d} points! "
                               "#kdctf #challengesolved #firstblood #cyber").format(
                               user.name, chal.name, place, score)
-                tweet_solve(tweet_text)
+                _tweet_solve(tweet_text)
 
-            play_teamsound(user.id)
+            _play_teamsound(user.id)
         except Exception as e:
             logger = logging.getLogger('tweetdfd')
             logger.exception("Tweet Announcement or team sound failed")
@@ -235,38 +239,46 @@ class TweetnamicValueChallenge(BaseChallenge):
         :return:
         """
         data = request.form or request.get_json()
-        submission = data['submission'].strip()
+        submission = data["submission"].strip()
         wrong = Fails(
             user_id=user.id,
             team_id=team.id if team else None,
             challenge_id=challenge.id,
             ip=get_ip(request),
-            provided=submission
+            provided=submission,
         )
-
         db.session.add(wrong)
         db.session.commit()
         db.session.close()
 
 
 class TweetnamicChallenge(Challenges):
-    __mapper_args__ = {'polymorphic_identity': 'tweetnamic'}
-    id = db.Column(None, db.ForeignKey('challenges.id'), primary_key=True)
+    __mapper_args__ = {"polymorphic_identity": "tweetnamic"}
+    id = db.Column(None, db.ForeignKey("challenges.id"), primary_key=True)
     initial = db.Column(db.Integer, default=0)
     minimum = db.Column(db.Integer, default=0)
     decay = db.Column(db.Integer, default=0)
 
     def __init__(self, *args, **kwargs):
         super(TweetnamicChallenge, self).__init__(**kwargs)
-        self.initial = kwargs['value']
+        self.initial = kwargs["value"]
 
-def tweet_solve(text):
+
+def load(app):
+    # upgrade()
+    app.db.create_all()
+    CHALLENGE_CLASSES["tweetnamic"] = TweetnamicValueChallenge
+    register_plugin_assets_directory(
+        app, base_path="/plugins/TweetTFd/assets/"
+    )
+
+def _tweet_solve(text):
     AUTH = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     AUTH.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     API = tweepy.API(AUTH)
     API.update_status(status=text)
 
-def play_teamsound(id):
+def _play_teamsound(id):
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_sock.settimeout(2)
 
@@ -278,8 +290,17 @@ def play_teamsound(id):
         print('closing socket')
         client_sock.close()
 
-def load(app):
-    # upgrade()
-    app.db.create_all()
-    CHALLENGE_CLASSES['tweetnamic'] = TweetnamicValueChallenge
-    register_plugin_assets_directory(app, base_path='/plugins/TweetTFd/assets/')
+def _getSolves(chal):
+    Model = get_model()
+
+    solve_count = (
+        Solves.query.join(Model, Solves.account_id == Model.id)
+        .filter(
+            Solves.challenge_id == chal.id,
+            Model.hidden == False,
+            Model.banned == False,
+        )
+        .count()
+    )
+
+    return solve_count
