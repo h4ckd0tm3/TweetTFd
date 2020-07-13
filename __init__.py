d7f96aae638b7e78c79da829d3213e23e38c4d78
h4ckd0tm3 <marcel@schnideritsch.at>
2020-07-13 08:47:33 +0200

    Discord Edition

--- __init__.py
+++ __init__.py
@@ -1,51 +1,54 @@
 from __future__ import division  # Use floating point for math calculations
-from CTFd.plugins.challenges import BaseChallenge, CHALLENGE_CLASSES
-from CTFd.plugins import register_plugin_assets_directory
-from CTFd.plugins.flags import get_flag_class
-from CTFd.models import (
-    db,
-    Solves,
-    Fails,
-    Flags,
-    Challenges,
-    ChallengeFiles,
-    Tags,
-    Hints,
-)
-from CTFd.utils.user import get_ip
-from CTFd.utils.uploads import delete_file
-from CTFd.utils.modes import get_model
-from flask import Blueprint
+
 import math
-import tweepy
-import sys
-import logging
-import socket
-from .config import *
+import request as rq
+
+from flask import Blueprint
+
+from CTFd.models import Challenges, Solves, db
+from CTFd.plugins import register_plugin_assets_directory
+from CTFd.plugins.challenges import CHALLENGE_CLASSES, BaseChallenge
+from CTFd.plugins.migrations import upgrade
+from CTFd.utils.modes import get_model
+
+
+class TweetnamicChallenge(Challenges):
+    __mapper_args__ = {"polymorphic_identity": "dynamic"}
+    id = db.Column(
+        db.Integer, db.ForeignKey("challenges.id", ondelete="CASCADE"), primary_key=True
+    )
+    initial = db.Column(db.Integer, default=0)
+    minimum = db.Column(db.Integer, default=0)
+    decay = db.Column(db.Integer, default=0)
+
+    def __init__(self, *args, **kwargs):
+        super(TweetnamicChallenge, self).__init__(**kwargs)
+        self.initial = kwargs["value"]
 
 
 class TweetnamicValueChallenge(BaseChallenge):
-    id = "tweetnamic"  # Unique identifier used to register challenges
-    name = "tweetnamic"  # Name of a challenge type
+    id = "dynamic"  # Unique identifier used to register challenges
+    name = "dynamic"  # Name of a challenge type
     templates = {  # Handlebars templates used for each aspect of challenge editing & viewing
-        "create": "/plugins/TweetTFd/assets/create.html",
-        "update": "/plugins/TweetTFd/assets/update.html",
-        "view": "/plugins/TweetTFd/assets/view.html",
+        "create": "/plugins/dynamic_challenges/assets/create.html",
+        "update": "/plugins/dynamic_challenges/assets/update.html",
+        "view": "/plugins/dynamic_challenges/assets/view.html",
     }
     scripts = {  # Scripts that are loaded when a template is loaded
-        "create": "/plugins/TweetTFd/assets/create.js",
-        "update": "/plugins/TweetTFd/assets/update.js",
-        "view": "/plugins/TweetTFd/assets/view.js",
+        "create": "/plugins/dynamic_challenges/assets/create.js",
+        "update": "/plugins/dynamic_challenges/assets/update.js",
+        "view": "/plugins/dynamic_challenges/assets/view.js",
     }
     # Route at which files are accessible. This must be registered using register_plugin_assets_directory()
-    route = "/plugins/TweetTFd/assets/"
+    route = "/plugins/dynamic_challenges/assets/"
     # Blueprint used to access the static_folder directory.
     blueprint = Blueprint(
-        "TweetTFd",
+        "dynamic_challenges",
         __name__,
         template_folder="templates",
         static_folder="assets",
     )
+    challenge_model = TweetnamicChallenge
 
     @classmethod
     def calculate_value(cls, challenge):
@@ -83,24 +86,8 @@ class TweetnamicValueChallenge(BaseChallenge):
         db.session.commit()
         return challenge
 
-    @staticmethod
-    def create(request):
-        """
-        This method is used to process the challenge creation request.
-
-        :param request:
-        :return:
-        """
-        data = request.form or request.get_json()
-        challenge = TweetnamicChallenge(**data)
-
-        db.session.add(challenge)
-        db.session.commit()
-
-        return challenge
-
-    @staticmethod
-    def read(challenge):
+    @classmethod
+    def read(cls, challenge):
         """
         This method is in used to access the data of a challenge in a format processable by the front end.
 
@@ -121,16 +108,16 @@ class TweetnamicValueChallenge(BaseChallenge):
             "max_attempts": challenge.max_attempts,
             "type": challenge.type,
             "type_data": {
-                "id": TweetnamicValueChallenge.id,
-                "name": TweetnamicValueChallenge.name,
-                "templates": TweetnamicValueChallenge.templates,
-                "scripts": TweetnamicValueChallenge.scripts,
+                "id": cls.id,
+                "name": cls.name,
+                "templates": cls.templates,
+                "scripts": cls.scripts,
             },
         }
         return data
 
-    @staticmethod
-    def update(challenge, request):
+    @classmethod
+    def update(cls, challenge, request):
         """
         This method is used to update the information associated with a challenge. This should be kept strictly to the
         Challenges table and any child tables.
@@ -149,154 +136,23 @@ class TweetnamicValueChallenge(BaseChallenge):
 
         return TweetnamicValueChallenge.calculate_value(challenge)
 
-    @staticmethod
-    def delete(challenge):
-        """
-        This method is used to delete the resources used by a challenge.
-
-        :param challenge:
-        :return:
-        """
-        Fails.query.filter_by(challenge_id=challenge.id).delete()
-        Solves.query.filter_by(challenge_id=challenge.id).delete()
-        Flags.query.filter_by(challenge_id=challenge.id).delete()
-        files = ChallengeFiles.query.filter_by(challenge_id=challenge.id).all()
-        for f in files:
-            delete_file(f.id)
-        ChallengeFiles.query.filter_by(challenge_id=challenge.id).delete()
-        Tags.query.filter_by(challenge_id=challenge.id).delete()
-        Hints.query.filter_by(challenge_id=challenge.id).delete()
-        TweetnamicChallenge.query.filter_by(id=challenge.id).delete()
-        Challenges.query.filter_by(id=challenge.id).delete()
-        db.session.commit()
-
-    @staticmethod
-    def attempt(challenge, request):
-        """
-        This method is used to check whether a given input is right or wrong. It does not make any changes and should
-        return a boolean for correctness and a string to be shown to the user. It is also in charge of parsing the
-        user's input from the request itself.
-
-        :param challenge: The Challenge object from the database
-        :param request: The request the user submitted
-        :return: (boolean, string)
-        """
-        data = request.form or request.get_json()
-        submission = data["submission"].strip()
-        flags = Flags.query.filter_by(challenge_id=challenge.id).all()
-        for flag in flags:
-            if get_flag_class(flag.type).compare(flag, submission):
-                return True, "Correct"
-        return False, "Incorrect"
-
-    @staticmethod
-    def solve(user, team, challenge, request):
-        """
-        This method is used to insert Solves into the database in order to mark a challenge as solved.
-
-        :param team: The Team object from the database
-        :param chal: The Challenge object from the database
-        :param request: The request the user submitted
-        :return:
-        """
-        challenge = TweetnamicChallenge.query.filter_by(id=challenge.id).first()
-        data = request.form or request.get_json()
-        submission = data["submission"].strip()
-
-        solve_count = _getSolves(challenge)
-
-        solve = Solves(
-            user_id=user.id,
-            team_id=team.id if team else None,
-            challenge_id=challenge.id,
-            ip=get_ip(req=request),
-            provided=submission,
-        )
-        db.session.add(solve)
-        db.session.commit()
+    @classmethod
+    def solve(cls, user, team, challenge, request):
+        super().solve(user, team, challenge, request)
 
         TweetnamicValueChallenge.calculate_value(challenge)
 
-        try:
-            if ENABLE_TWEET and solve_count == 0:
-                score = user.get_score(admin=True)
-                place = user.get_place(admin=True)
-                tweet_text = ("{} got first blood on {} and "
-                            "is now in {} place with {:d} points! "
-                            "#kdctf #challengesolved #firstblood #cyber").format(
-                            user.name, challenge.name, place, score)
-                _tweet_solve(tweet_text)
-            if ENABLE_TEAMSOUND:
-                _play_teamsound(user.id)
-        except Exception as e:
-            try:
-                logger = logging.getLogger('tweetdfd')
-                logger.exception("Tweet Announcement or team sound failed")
-            except Exception:
-                # everything is lost
-                pass
-
-    @staticmethod
-    def fail(user, team, challenge, request):
-        """
-        This method is used to insert Fails into the database in order to mark an answer incorrect.
-
-        :param team: The Team object from the database
-        :param challenge: The Challenge object from the database
-        :param request: The request the user submitted
-        :return:
-        """
-        data = request.form or request.get_json()
-        submission = data["submission"].strip()
-        wrong = Fails(
-            user_id=user.id,
-            team_id=team.id if team else None,
-            challenge_id=challenge.id,
-            ip=get_ip(request),
-            provided=submission,
-        )
-        db.session.add(wrong)
-        db.session.commit()
-        db.session.close()
+        if _getSolves(challenge) == 0:
 
 
-class TweetnamicChallenge(Challenges):
-    __mapper_args__ = {"polymorphic_identity": "tweetnamic"}
-    id = db.Column(None, db.ForeignKey("challenges.id"), primary_key=True)
-    initial = db.Column(db.Integer, default=0)
-    minimum = db.Column(db.Integer, default=0)
-    decay = db.Column(db.Integer, default=0)
-
-    def __init__(self, *args, **kwargs):
-        super(TweetnamicChallenge, self).__init__(**kwargs)
-        self.initial = kwargs["value"]
 
 
 def load(app):
-    # upgrade()
-    app.db.create_all()
+    upgrade()
     CHALLENGE_CLASSES["tweetnamic"] = TweetnamicValueChallenge
     register_plugin_assets_directory(
         app, base_path="/plugins/TweetTFd/assets/"
     )
-
-def _tweet_solve(text):
-    AUTH = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    AUTH.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    API = tweepy.API(AUTH)
-    API.update_status(status=text)
-
-def _play_teamsound(id):
-    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_sock.settimeout(2)
-
-    try:
-        client_sock.connect(("10.80.{:d}.100".format(id), 1338))
-        message = b'kdctf_play'
-        client_sock.sendall(message)
-    finally:
-        print('closing socket')
-        client_sock.close()
 
 def _getSolves(chal):
     Model = get_model()
